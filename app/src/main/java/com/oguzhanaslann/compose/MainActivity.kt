d2b9e01d451afaafa8c15b0a974fbae92d0aa20d
oguzhanaslann <oguz21199@gmail.com>
2025-10-04 22:32:29 +0300

    feat: Enhance autofill example and update gitignore
    This commit improves the `AutofillTextField` example by specifying the keyboard type.
    
    - Sets the `keyboardType` to `KeyboardType.Email` for a better user experience.
    - Adds `auto_fill_notes.md` and `auto_fill.md` to the `.gitignore` file.

--- app/src/main/java/com/oguzhanaslann/compose/MainActivity.kt
+++ app/src/main/java/com/oguzhanaslann/compose/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -31,6 +32,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedSecureTextField
@@ -193,7 +195,10 @@ private fun AutofillTextField() {
                 .semantics {
                     contentType = ContentType.EmailAddress
                 },
-            placeholder = { Text("Autofill Field") }
+            placeholder = { Text("Autofill Field") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email
+            )
         )
 
         Spacer(modifier = Modifier.padding(4.dp))
